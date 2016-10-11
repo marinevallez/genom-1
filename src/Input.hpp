f5b9bbd98ab1af3374e4d9c0277e496adcb3c997
arsinev <arseniy.sinev@epfl.ch>
2016-10-11 14:25:11 +0200

    modification of the sequence class
    1. searchMotif method can search both sequences + it looks for a 7
    letter match within the sequence
    2. Input class created as purely virtual to serve a super class for
    future classes
    3. Useless methods were removed from the Sequence class

---
+++ src/Input.hpp
@@ -0,0 +1,17 @@
+#ifndef INPUT_H
+#define INPUT_H
+#include <iostream>
+using namespace std;
+
+class Input		//a purely virtual class because we will never have JUST input, it will always be a specific sub-class
+{
+public:
+
+	Input() {};	//constructor and destructor
+	~Input() {};
+	virtual void loadFile(string) = 0;	//a purely virtual method for loading files that requires a file name 
+										//need to be redefined in every sub-class
+
+};
+
+#endif
