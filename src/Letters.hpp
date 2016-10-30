83b283ba0b67ae3d182f9a8abcfdf44d485c3701
Oriane Peter <oriane.peter@epfl.ch>
2016-10-30 13:04:46 +0100

    Added comment + optimization

--- src/Letters.hpp
+++ src/Letters.hpp
@@ -1,10 +1,4 @@
-//
-//  Letters.hpp
-//  partie 5
-//
-//  Created by Oriane Peter on 24.10.16.
-//  Copyright © 2016 Oriane Peter. All rights reserved.
-//
+
 
 #ifndef Letters_hpp
 #define Letters_hpp
@@ -12,7 +6,13 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <stdio.h>
-#include <array>
+
+
+struct Motif
+{
+    std::vector<char> lettres;
+    double binding_score;
+};
 
 using namespace std;
 
@@ -20,14 +20,15 @@ class Letters
 {
 public:
     void draw(sf::RenderTarget& target);
-    Letters();
-    void calculate_size(vector<double> tailles);
-    array<double, 4> get_lettre_size();
-
-private :
+    Letters(vector<Motif> motif_); // the constructor process the size that should be given to a lettre given the probability of apperance at a certain position of the sequence.
+    vector<vector<double>> get_lettre_size(); // returns  Letters_size
+    sf::RectangleShape set_text( double taille, int lettre, double emplacement, int largeur); // determine the textrure the position and the size of a lettre
+    double size( int ltr, Letters tentative);
+    private :
     sf::Texture texture;
     sf::Sprite sprite;
-    array<double, 4> tailles_lettre; // 4 case qui représente les tailles de dans cette ordre ACGT
+    vector<vector<double>> Letters_size; // 4 column for every lettre in the ACGT order, the number of row corresponding to the size of the analysed sequences.
+    
 };
 
 #endif /* Letters_hpp */
