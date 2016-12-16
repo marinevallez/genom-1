32c3d5a12a14aed046b8885be5dd7ade8cf28339
Guillaume Pillet <guillaume.pillet@epfl.ch>
2016-12-16 11:27:34 +0100

    Modified the comments to Doxyfile format

--- src/Logo.hpp
+++ src/Logo.hpp
@@ -18,15 +18,44 @@
 using namespace std;
 
 class Logo {
-    array<sf::Texture, 4> lettre_texture; // contain the texture of each letter filled with the emplacment of the image by the constructor
+
+/*!
+ * contain the texture of each letter filled with the emplacment of the image by the constructory.
+ * */
+ 
+    array<sf::Texture, 4> lettre_texture; 
    
     
 public:
-    Logo(); // fills lettre_texture
-    int afficher_logo(vector<Pattern> sequences, matrix); // Display the logo
-    double size( double size_b); // calculates the size that must be displayed based on the calculation of the size depending on its probability (this function calculates a abritrary size and might result in  a lost of information !)
-    sf::RectangleShape set_text( double taille, size_t lettre, double emplacement, int largeur); // for a given letter gives the appropriate size, texture and emplacment.
-    vector<size_t> Letters_order(Letters Letters_, size_t j); // determine the order of probability for a given place on the sequence.
+
+/*!
+ * Fills lettre_texture.
+ * */
+    Logo();
+   
+/*!
+ * Display the logo.
+ * */
+    int afficher_logo(vector<Pattern> sequences, matrix); 
+    
+   
+/*!
+ * Calculates the size that must be displayed, based on its probability. This function calculates a arbitrary size and might result in a loss of information !
+ * */
+    double size( double size_b);
+
+
+/*!
+ * For a given letter, gives the appropriate size, texture and emplacement.
+ * */
+    sf::RectangleShape set_text( double taille, size_t lettre, double emplacement, int largeur); 
+    
+    
+
+/*!
+ * Determine the order of probability for a given place in the sequence.
+ * */    
+    vector<size_t> Letters_order(Letters Letters_, size_t j); 
     
 };
 
